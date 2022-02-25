0ff71c7ab5672330589890a91e963bdf0a1914a3
Sam Howard <samhoward1990@gmail.com>
2022-02-25 14:45:21 -0600

    added goal text file to guide progress and work

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, redirect
 from mysqlconnection import MySQLConnector
 app = Flask(__name__)
-mysql = MySQLConnector(app, 'fandom_pokemon_db')
+mysql = MySQLConnector(app, 'fandom_pokemon_db_test')
 
 @app.route("/")
 def index():
@@ -12,7 +12,11 @@ def getAllUsers():
     query = "SELECT * FROM users"
     allUsers = mysql.query_db('SELECT * FROM users')
     print(allUsers)
-    return redirect("/")
+    users = {}
+    for user in allUsers:
+        users[user['email']] = user
+    return users
+
 
 @app.route("/register", methods=['POST'])
 def register():
@@ -22,6 +26,16 @@ def register():
         'username': request.form['username'],
         'password': request.form['password']
     }
+    newUser = mysql.query_db(query, data)
+    return newUser
+
+@app.route('/createcharacter', methods=['POST'])
+def createCharacter():
+    query = 'INSERT INTO characters (name, created_at, updated_at) VALUES (:name, NOW(), NOW())'
+    data = {
+        'name': request.form['name'],
+        'fandom': request.form['fandom']
+    }
     mysql.query_db(query, data)
-    return redirect("/")
+    return redirect('/')
 app.run(debug=True)
