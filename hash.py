6f456aa9c2008130ff25a93a960d9862aba23368
TatersTot <jack.lawton1@yahoo.com>
2022-03-17 22:47:24 -0400

    Added examples of hash code succeeding and failing

---
+++ hash.py
@@ -0,0 +1,31 @@
+import hashlib
+import os, binascii
+
+def setPass(password, salt):
+    hashedPW = hashlib.md5("{}{}".format(password, salt).encode()).hexdigest()
+    return hashedPW
+
+def setPassAgain(password, salt):
+    hashedPW = hashlib.md5(f"{password},{salt}".encode()).hexdigest()
+    return hashedPW
+
+def setPassAgainButDifferent(password, salt):
+    hashedPW = hashlib.md5("{}{}".format(password, salt).encode()).hexdigest()
+    return hashedPW
+
+def setSalt():
+    return binascii.b2a_hex(os.urandom(15))
+
+salt = setSalt()
+passwordInfo = setPass("password", salt)
+passwordFailer = setPassAgain("password", salt)
+passwordMatcher = setPassAgainButDifferent("password", salt)
+
+print(passwordInfo)
+print(passwordMatcher)
+print(passwordFailer)
+
+print(passwordInfo == passwordMatcher)
+print(passwordInfo == passwordFailer)
+
+
